6b919925811dfb9888635f1c8adb0af759d22757
ehsan <1374ea@gmail.com>
2020-02-28 14:03:07 +0330

    add user admin

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,1 +1,19 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.models import User
+
+from apps.user.models import UserAdditionalData
+
+# unregistering django default user admin model
+admin.site.unregister(User)
+
+
+class UserAdditionalDataInline(admin.StackedInline):
+    model = UserAdditionalData
+    extra = 1
+    min_num = 1
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ('last_login', 'date_joined')
+    inlines = (UserAdditionalDataInline,)
--- apps/user/routers.py
+++ apps/user/routers.py
@@ -1,6 +1,4 @@
 from apps.user import viewsets
 from rest_workspace.routers import ROUTER
 
-ROUTER.register(r'users',
-                viewsets.UserViewSet,
-                basename='user')
+ROUTER.register(r'users', viewsets.UserViewSet, basename='user')
