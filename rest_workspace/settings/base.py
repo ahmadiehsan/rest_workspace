1e3c994498749a02464fadbf1816f3f90f2ce148
ehsan <1374ea@gmail.com>
2020-02-28 14:22:25 +0330

    update base settings

--- rest_workspace/settings/base.py
+++ rest_workspace/settings/base.py
@@ -16,9 +16,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '&)&_2g(v!#)6(q$qt%4nhkcc_uw!*&2xsd(ywnl=6!!e9$^fqz'
 
